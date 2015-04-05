5500f63028988add2a3f9ab7f5f8b14dfd72db3f
ch98-1 <akerunakayama+1@gmail.com>
2015-04-05 15:18:27 -0700

    Added border

--- main.c
+++ main.c
@@ -4,9 +4,6 @@
 
 int main(int argc, char *argv[]){
 
-	//initialise variables 
-	delay = DELAY;//10 ms delay
-
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0){//start SDL and get any error if it dosen't.
 		printf("Could not load SDL: %s\n", SDL_GetError());//print out error
 		exit(EXIT_FAILURE);//exit
@@ -18,7 +15,7 @@ int main(int argc, char *argv[]){
 	// load support for PNG image format
 	int flags = IMG_INIT_PNG;//flag for PNG image loading
 	int initted = IMG_Init(flags);//get initialised flags
-	if (initted&flags != flags) {//if it caused error
+	if ((initted&flags) != flags) {//if it caused error
 		printf("Could not initialised png support: %s\n", IMG_GetError());//print error message
 		exit(EXIT_FAILURE);//exit
 	}
@@ -74,7 +71,7 @@ int main(int argc, char *argv[]){
 
 
 	//initialise and load stuff
-
+	delay = DELAY;//10 ms delay
 	Resize();//resize stuff
 	menue = 0;//default no menue at start
 	message = 0;//no message at start
@@ -83,11 +80,21 @@ int main(int argc, char *argv[]){
 		menue = 1;//need to display menue
 		Message("Could not load save file");//send message that save file could not be loaded
 	}
+	objectssize = 0;//initialise array sizes
+	texturessize = 0;
+	pause = 0;//start without pausing
 
 
-	//test image 
-	SDL_Texture* texture = GetTexture("test.png");
-	SDL_Texture* text = GetTextTexture(font_32, "Heloooooo WORLD!!!!!!!!", 0, 255, 255);
+
+
+
+
+
+	//Load textures and objects
+	LoadObjects();
+
+
+
 
 
 	//main loop
@@ -98,22 +105,11 @@ int main(int argc, char *argv[]){
 				Quit();//quit everything
 				break;//get out
 			case SDL_MOUSEBUTTONDOWN://when clicking down
-				if (event.button.which != SDL_TOUCH_MOUSEID){//ignore touch event for later
 					Clicked(event.button.x - baseX, event.button.y - baseY);//run clicked function 
-				}
 				break;//get out
 			case SDL_MOUSEMOTION://when mouse moved
-				if (event.button.which != SDL_TOUCH_MOUSEID){//ignore touch event for later
 					MouseX = event.button.x - baseX;//set x and y position of mouse from square
 					MouseY = event.button.y - baseY;
-				}
-				break;//get out
-			case SDL_FINGERDOWN://when touching down
-				Clicked(event.button.x - baseX, event.button.y - baseY);//run clicked function 
-				break;//get out
-			case SDL_FINGERMOTION://when finger moved
-				MouseX = event.button.x - baseX;//set x and y position of mouse from square
-				MouseY = event.button.y - baseY;
 				break;//get out
 			default://for everything else
 				//ignore event
@@ -129,6 +125,7 @@ int main(int argc, char *argv[]){
 				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);//draw white
 				SDL_RenderClear(renderer);//clear screen
 				DrawText(messagetexture, CENTERED, CENTERED, NULL);//draw message at center
+				DrawEdge();//draw edge
 				SDL_RenderPresent(renderer);//update screen
 				message = 2;
 			}
@@ -153,10 +150,11 @@ int main(int argc, char *argv[]){
 						DrawText(menuetexture[i], CENTERED, y, NULL);
 						i++;//count i up
 					}
+
+					DrawEdge();//draw edge
 					SDL_RenderPresent(renderer);//update screen
 					menue = 2;
 				}
-
 
 			}
 
@@ -164,7 +162,7 @@ int main(int argc, char *argv[]){
 
 
 
-			else{//if in normal play mode
+			else if(!pause){//if in normal play mode and not in pause
 
 				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);//draw white
 				SDL_RenderClear(renderer);//clear screen
@@ -172,23 +170,23 @@ int main(int argc, char *argv[]){
 				DrawBase();//draw basic stuff for normal play
 
 
-				DrawIMG(texture, MouseX, MouseY, NULL, 0.25 , 0.25 , 1);//draw image centered at mouse
-				int x = baseX + ((int)((clock() * 100) / CLOCKS_PER_SEC)) % maxside;
+				DrawIMG(textures[0], MouseX, MouseY, NULL, 0.25, 0.25, 1);//draw image centered at mouse
+				int x = ((int)((clock() * 100) / CLOCKS_PER_SEC)) % maxside;
 				int y = baseY;
-				DrawText(text, x, baseY, NULL);//draw that text at top
+				DrawText(textures[1], x, baseY, NULL);//draw that text at top
 				for (y = 0; y < maxside; y += maxside / 32){
-					DrawText(text, x, y, NULL);//draw text
+					DrawText(textures[1], x, y, NULL);//draw text
 				}
+
+
+
+
+
+				DrawEdge();//draw edge
 				SDL_RenderPresent(renderer);//update screen
-
-
-
-
-
 			}
 
 		}
-
 
 
 		SDL_Delay(delay);
@@ -207,8 +205,6 @@ int main(int argc, char *argv[]){
 
 void Message(const char *text){//display message 
 	messagetexture = GetTextTexture(font_32, text, 0, 0, 0);//render black text of the message
-	int w, h, access;//value to fill up
-	long format;
 	message = 1;//set to display message
 }
 
@@ -226,6 +222,41 @@ int EventFilter(void* userdata, SDL_Event* e){//event filter
 		exit(EXIT_SUCCESS);//exit
 		return 0;//delete that event
 		break;//get out
+
+	case SDL_APP_TERMINATING://quit everything
+		exit(EXIT_SUCCESS);//exit
+		return 0;//delete that event
+		break;//get out
+
+	case SDL_APP_LOWMEMORY://if low in memory
+		//can't do anything
+		return 0;//delete that event
+		break;//get out
+
+	case SDL_APP_WILLENTERBACKGROUND://if entering background
+		Save();//save game
+		pause = 1;//pause game
+		delay = DELAY_SLOW;//lower fps
+		return 0;//delete that event
+		break;//get out
+
+	case SDL_APP_DIDENTERBACKGROUND://if in background
+		//don't do anything
+		return 0;//delete that event
+		break;//get out
+
+	case SDL_APP_WILLENTERFOREGROUND://if entering forgeound
+		LoadFile(SAVE);//load game
+		pause = 0;//unpause pause game
+		delay = DELAY;//higher fps
+		return 0;//delete that event
+		break;//get out
+
+	case SDL_APP_DIDENTERFOREGROUND://if in forground
+		//don't do anything
+		return 0;//delete that event
+		break;//get out
+
 	default://for everything else
 		return 1;//don't delete event
 		break;//get out
@@ -240,6 +271,26 @@ int EventFilter(void* userdata, SDL_Event* e){//event filter
 
 void Quit(void){//quit everything
 
+	Save();//save file before quitting
+	TTF_CloseFont(font_4);//close all fonts
+	TTF_CloseFont(font_6);
+	TTF_CloseFont(font_8);
+	TTF_CloseFont(font_16);
+	TTF_CloseFont(font_24);
+	TTF_CloseFont(font_32);
+	TTF_CloseFont(font_46);
+	TTF_CloseFont(font_64);
+	SDL_DestroyRenderer(renderer);//destroy renderer
+	int i;//counter
+	for (i = 0; i < texturessize / sizeof(SDL_Texture*); i++){//destroy each texture
+		SDL_DestroyTexture(textures[i]);//destroy texture
+	}
+	for (i = 0; i < 28; i++){//destroy each menue texture
+		SDL_DestroyTexture(menuetexture[i]);//destroy texture
+	}
+	free(textures);//free textures array
+	free(objects);//free objects array
+	SDL_DestroyWindow(window);//destroy window
 	IMG_Quit();//quit SDL_Image
 	TTF_Quit();//quit SDL_TTF
 	SDL_Quit();//quit SDL
@@ -282,7 +333,7 @@ void GetDisplay(void){//get display
 void Clicked(long int x, long int y){//x and y positions clicked
 	if (menue && !message && x > 0 && x < maxside){//if in menue and not in message and in right area
 		int clicked = y / (maxside / 32);//which button was clicked
-		if (clicked > 2 & clicked < 29){ //if within the range of levels
+		if (clicked > 2 && clicked < 29){ //if within the range of levels
 			map = clicked - 3;//get clicked button
 			menue = LoadFile(maps[map][1]);//load that level
 		}
@@ -290,7 +341,7 @@ void Clicked(long int x, long int y){//x and y positions clicked
 			menue = 0;//stop showing menue
 		}
 	}
-	else{//if in normal play mode
+	else if(!pause){//if in normal play mode and not paused
 
 	}
 
@@ -320,6 +371,7 @@ SDL_Texture* GetTexture(const char *file){//make texture from this file
 		printf("could not create texture: %s\n", SDL_GetError());//error message
 		exit(EXIT_FAILURE);//exit
 	}
+	SDL_FreeSurface(surface);//free surface
 	return texture;//return texture
 }
 
@@ -349,7 +401,12 @@ TTF_Font* GetFont(const char *file, int size){//get font from file
 
 
 SDL_Texture* GetTextTexture(TTF_Font* font, const char* text, int r, int g, int b){//make texture from font text and rgb
-	SDL_Surface* surface = TTF_RenderText_Blended(font, text, (SDL_Color){ r, g, b });//rendere text as surface
+	SDL_Color color;//set color
+	color.r = r;
+	color.g = g;
+	color.b = b;
+	color.a = 255;
+	SDL_Surface* surface = TTF_RenderText_Blended(font, text, color);//rendere text as surface
 	if (surface == NULL){//if it could not be loaded
 		printf("could not load text: %s\n", TTF_GetError());//error message
 		exit(EXIT_FAILURE);//exit
@@ -397,17 +454,24 @@ void Resize(void){//recalculate numbers related to size
 
 
 void DrawBase(void){//draw basic stuff
-	SDL_Rect rect;//rectangle to draw with
-
-	//border square
-	rect.w = maxside;
-	rect.h = maxside;
-	rect.x = baseX;
-	rect.y = baseY;
-
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);//draw border suare in black
-	SDL_RenderDrawRect(renderer, &rect);
 }
+
+
+
+
+
+
+
+
+
+
+
+void DrawEdge(void){//draw edge border of screen
+	DrawIMG(textures[2], CENTERED, CENTERED, NULL, 2, 2, 1);//centered at twice the size of box
+}
+
+
+
 
 
 
@@ -482,10 +546,13 @@ void DrawText(SDL_Texture *texture, int x, int y, SDL_Rect *rect){//draw rect of
 	SDL_QueryTexture(texture, &format, &access, &w, &h);//get text box size
 	dest.w = (int) w;//set width and height
 	dest.h = (int) h;
-	dest.x = x + baseX;//set x and y
-	dest.y = y + baseY;
-	if (x == CENTERED) dest.x = (maxside / 2 - w / 2) + baseX;//center it if set to centered
-	if (y == CENTERED) dest.y = (maxside / 2 - h / 2) + baseY;
+	dest.x = x;//set x and y
+	dest.y = y;
+	if (x == CENTERED) dest.x = (maxside / 2 - w / 2);//center it if set to centered
+	if (y == CENTERED) dest.y = (maxside / 2 - h / 2);
+
+	dest.x += baseX;//set origin to basex and y
+	dest.y += baseY;
 
 	SDL_RenderCopy(renderer, texture, rect, &dest);//draw texture
 
@@ -502,20 +569,121 @@ void DrawText(SDL_Texture *texture, int x, int y, SDL_Rect *rect){//draw rect of
 
 void DrawIMG(SDL_Texture *texture, int x, int y, SDL_Rect *rect, double w, double h, int center){//draw rect of texture at x and y position at scale from maxside. Null rect for whole texture. -32000 or CENTERED to get that axis centered. set center to 1 to center to x and y
 	SDL_Rect dest;
-	dest.w = (int)maxside * w;//set width and height
-	dest.h = (int)maxside * h;
+	dest.w = (int) (maxside * w);//set width and height
+	dest.h = (int) (maxside * h);
 	dest.x = x;//set x and y
 	dest.y = y;
+	if (x == CENTERED) dest.x = (int)(maxside / 2 - w / 2);//center it if set to centered
+	if (y == CENTERED) dest.y = (int)(maxside / 2 - h / 2);
 	if (center){
-		dest.x = x - dest.w / 2 + baseX;//set x and y centered to x and y
-		dest.y = y - dest.h / 2 + baseY;
+		dest.x = dest.x - dest.w / 2;//set x and y centered to x and y
+		dest.y = dest.y - dest.h / 2;
 	}
-	if (x == CENTERED) dest.x = (maxside / 2 - w / 2) + baseX;//center it if set to centered
-	if (y == CENTERED) dest.y = (maxside / 2 - h / 2) + baseY;
+
+	dest.x += baseX;//set origin to basex and y
+	dest.y += baseY;
 
 	SDL_RenderCopy(renderer, texture, rect, &dest);//draw texture
 
 }
+
+
+
+
+
+
+
+
+
+
+
+void LoadObjects(void){//load all objects
+	//add test textures
+	AddTexture(GetTexture("test.png"));//0
+	AddTexture(GetTextTexture(font_32, "Heloooooo WORLD!!!!!!!!", 0, 255, 255));//1
+	AddTexture(GetTexture("border.png"));//2
+
+}
+
+
+
+
+
+
+
+
+
+
+unsigned int AddTexture(SDL_Texture *texture){//add texture to memory and return that id
+	texturessize += sizeof(SDL_Texture*);//update texturessize
+	textures = realloc(textures, texturessize);//reallocate memory for longer array
+	if (textures == NULL){//if it becomes null
+		printf("could not allocate new memory for textures\n");//print out error if it happens
+		exit(EXIT_FAILURE);//exit
+	}
+	textures[(texturessize / sizeof(SDL_Texture*))-1] = texture;//set value
+	return (texturessize / sizeof(SDL_Texture*))-1;//texture adress
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+unsigned int AddObject(Object *object){//add object to memory and return that id
+	objectssize += sizeof(Object);//update objectssize
+	objects = realloc(objects, objectssize);//reallocate memory for longer array
+	if (objects == NULL){//if it becomes null
+		printf("could not allocate new memory for object\n");//print out error if it happens
+		exit(EXIT_FAILURE);//exit
+	}
+	objects[(objectssize / sizeof(Object*))-1] = object;//set value
+	return (objectssize / sizeof(Object*))-1;//object adress
+
+}
+
+
+
+
+
+
+
+
+
+
+Object *MakeObject(int iw, int ih, int frames, SDL_Texture *texture, Button button, Button status, Button update, double w, double h, int center, int selectable){//make object with that paramiters
+	Object *object = malloc(sizeof(Object));//make that object
+	object->iw = iw;//set each variable
+	object->ih = ih;
+	object->frames = frames;
+	object->texture = texture;
+	object->button = button;
+	object->status = status;
+	object->update = update;
+	object->w = w;
+	object->h = h;
+	object->center = center;
+	object->selectable = selectable;
+
+	return object;//return the created object
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
--- main.h
+++ main.h
@@ -15,6 +15,7 @@
 
 #define WINDOW_NAME "Trash Tower Defence"
 #define DELAY 10 //default delay
+#define DELAY_SLOW 10000 //dealy while in background
 #define SLEEP_DELAY 1000 //delay at sleep
 #define CENTERED -32000 //center x and y in draw function
 
@@ -46,8 +47,40 @@ double money, landfill, speed;//status of game
 long int month;
 int menue;//if there is menue on screen 0 for normal play, 1 for displaying menue, 2 for menue already displayd
 int map;//which one of 26 map to use
+int pause;//if game is paused or not
 
 
+
+
+
+typedef struct data{
+	int oid;//object id
+	int frame;//which frame it is in
+	int x, y;//x and y position
+	struct data *next;//linked list
+}Data;
+
+typedef void(*Button)(Data *);//function for buttons
+
+typedef struct object{//array of clickable object definitions
+	int iw, ih;//width and height of image to display
+	int frames;//number of frames
+	SDL_Texture *texture;//id of pointer to texture
+	Button button;//button function to run if clicked on
+	Button status;//id of function to display status bar while selected
+	Button update;//id of function to update every frame
+	double w, h;//width, and hight in scale from maxside
+	int center;//if centering on x and y or not
+	int selectable;//if selectable or not
+}Object;
+
+Object **objects;//object defenitions
+unsigned long int objectssize;//size of objects array
+
+SDL_Texture **textures;//array of texture pointers
+unsigned long int texturessize;//size of textures array
+
+Data *start;//first object to draw in the linked list
 
 uint32_t delay;//amount of delay for each frame
 long int MouseX, MouseY;//x and y position of mouse / touch
@@ -67,7 +100,7 @@ TTF_Font* font_64;
 int EventFilter(void* userdata, SDL_Event* e);//event filter
 void Quit(void);//quit everything
 void GetDisplay(void);//get display
-void Clicked(int x, int y);//x and y positions clicked
+void Clicked(long int x, long int y);//x and y positions clicked
 void Resize(void);//recalculate numbers related to size
 SDL_Texture* GetTexture(const char *file);//make texture from this file
 TTF_Font* GetFont(const char *file, int size);//get font from file
@@ -79,5 +112,11 @@ void Save(void);//save in to save file
 void LoadMenue(void);//load menue texts
 void DrawText(SDL_Texture *texture, int x, int y, SDL_Rect *rect);//draw rect of texture at x and y position. Null rect for whole texture. -32000 or CENTERED to get that axis centered
 void DrawIMG(SDL_Texture *texture, int x, int y, SDL_Rect *rect, double w, double h, int center);//draw rect of texture at x and y position at scale from maxside. Null rect for whole texture. -32000 or CENTERED to get that axis centered. set center to 1 to center to x and y
+void LoadObjects(void);//load all objects
+void DrawEdge(void);//draw edge border of screen
+unsigned int AddTexture(SDL_Texture *texture);//add texture to memory and return that id
+unsigned int AddObject(Object *object);//add object to memory and return that id
+Object *MakeObject(int iw, int ih, int frames, SDL_Texture *texture, Button button, Button status, Button update, double w, double h, int center, int selectable);//make object with that paramiters
+
 
 #endif
